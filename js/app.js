9956caa8a244fcc53d5d892184b9a14277196ad4
Kishan <kkpatel911@gmail.com>
2018-12-29 17:25:22 -0500

    added comments

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
-
-
-$('.spoiler').on('click','button', function(event){
-console.log(event.target);
-$(event.target).prev().show();
-
-$(event.target).hide();
+$('.spoiler').on('click', 'button', function (event) {
+    //Show the spoiler text
+    $(event.target).prev().show();
+    //hide the button
+    $(event.target).hide();
 });
-
+//create the button that says Reveal Spoiler
 var $button = $('<button>Reveal Spoiler</button>');
+// Add the button to the spoiler class
 $('.spoiler').append($button);
-
+//Hide the spoiler text inside a button
 $('.spoiler span').hide();
